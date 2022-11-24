9fe40c60962c416f31d5cbd0a1441b3161a45cba
ogiogiovictor@gmail.com <ogiogiovictor@gmail.com>
2022-11-24 05:18:25 -0600

    fixed issues with migration file

--- db/migrate/20221123092506_create_likes.rb
+++ db/migrate/20221123092506_create_likes.rb
@@ -1,11 +1,11 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.integer :authorid
-      t.text :postid
+      t.integer :author_id
+      t.text :post_id
       t.timestamps
     end
 
-    add_index :likes, [:authorid, :postid]
+    add_index :likes, [:author_id, :post_id]
   end
 end
--- db/migrate/20221123092555_create_comments.rb
+++ db/migrate/20221123092555_create_comments.rb
@@ -1,13 +1,12 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.integer :authorid
-      t.integer :postid
+      t.integer :author_id
+      t.integer :post_id
       t.text :text
-      t.text :postcounter
       t.timestamps
     end
 
-    add_index :comments, [:authorid, :postid]
+    add_index :comments, [:author_id, :post_id]
   end
 end
