d0ddd4cdcc7e58ae018981e5904927ec684c26b1
ogiogiovictor@gmail.com <ogiogiovictor@gmail.com>
2022-11-29 14:22:25 -0600

    added controllers for users and post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+class PostsController < ApplicationController
+
+    def index
+        #@user = User.find(params[:user_id])
+        #@posts = Post.where(user_id: params[@user.id])
+        @posts = Post.where(author_id: params[:user_id])
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+end
+
+
+ 
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+
+  def index
+    @user = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+ end
+
+end
