145a0684aafa75f39df74205101803b5fe899fb6
ogiogiovictor@gmail.com <ogiogiovictor@gmail.com>
2022-12-01 16:35:16 -0600

    created views for post and users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,12 @@
 class PostsController < ApplicationController
   def index
-    # @user = User.find(params[:user_id])
+     @user = User.find(params[:user_id])
     # @posts = Post.where(user_id: params[@user.id])
     @posts = Post.where(author_id: params[:user_id])
   end
 
   def show
+    @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
 end
