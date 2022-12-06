c4f0f1440a1fbd24f2c2b46b5dc942aa822929a4
Ukaypromise <promiseukaimo@gmail.com>
2022-12-06 10:33:53 -0500

    Add user and posts to test user show

--- spec/integration/user_show_spec.rb
+++ spec/integration/user_show_spec.rb
@@ -1,11 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe 'user_path', type: :system do
+RSpec.describe 'User Show', type: :feature do
+    before(:each) do
+    @user = User.create(
+      name: 'Promise',
+      bio: 'Fullstack Developer',
+      photo: 'http://promise.com',
+      posts_counter: 2
+      )
+
+    @post1 = Post.create(
+      author: @user,
+      title: 'First post',
+      text: 'This is my first post',
+      comments_counter: 1,
+      likes_counter: 3
+    )
+
+    @post2 = Post.create(
+      author: @user,
+      title: 'Second post',
+      text: 'This is my second post',
+      comments_counter: 2,
+      likes_counter: 2
+    )
+  end
     describe 'user show pages' do
-        # it 'shows the user username' do
-        #     visit user_path(User.first)
-        #     expect(page).to have_content('Tom')
-        # end
+       
     end
 
 end
