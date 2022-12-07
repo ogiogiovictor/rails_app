81c590848716d5104e76a8d8af6fa4bf61375b46
ogiogiovictor@gmail.com <ogiogiovictor@gmail.com>
2022-12-07 15:50:16 -0600

    fixed linter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # has_many :posts, class_name: 'post', foreign_key: 'author_id'
-   # Include default devise modules. Others available are:
+  # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
