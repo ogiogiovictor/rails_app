ee86cedb8078f994e84edb36c312b62bcee38262
Ukaypromise <promiseukaimo@gmail.com>
2022-12-09 15:14:51 -0500

    Add method to list all post

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,12 +2,25 @@ class Api::V1::PostsController < ApplicationController
   # before_action :authorize_request
   before_action :find_user
 
+
+
+  def index
+    post=Post.where(author_id:params[:user_id])
+    if post
+      render json:post, status: :ok
+    else
+      render json:{message:'No post found'}, status: :not_found
+    end
+  end
+
   def show
     @post = @user.posts.find_by_id!(params[:id])
     render json: @post, status: :ok
   rescue ActiveRecord::RecordNotFound
     render json: { message: 'Post not found' }, status: :not_found
   end
+
+
 
   private
 
