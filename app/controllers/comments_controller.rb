3df1395d5ebedb2a9925e753391abfc67ef0ce31
ogiogiovictor@gmail.com <ogiogiovictor@gmail.com>
2022-12-09 01:37:28 -0600

    modified user model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,14 +4,6 @@ class CommentsController < ApplicationController
     @user = current_user
   end
 
-  def destroy
-    @comment = Comment.find(params[:id])
-    @post = Post.find(@comment.post_id)
-    @post.decrement!(:comments_count)
-    @comment.destroy
-
-    redirect_to root_path, status: :see_other
-  end
 
   def create
     @post = Post.find(params[:post_id] || params[:id])
