0e5b37e50165ba60eb57ae3b2ca5f7e280f62215
ogiogiovictor@gmail.com <ogiogiovictor@gmail.com>
2022-12-10 06:16:34 -0600

    remove rwag

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  #mount Rswag::Ui::Engine => '/api-docs'
+  #mount Rswag::Api::Engine => '/api-docs'
   devise_for :users, sign_out_via: [:get, :post]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
